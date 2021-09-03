9d4b3810b4d9ec65a5bde0c027ca294c01049a21
pmontens <pieterjan@montens.net>
2021-09-03 22:13:16 +0200

    :wrench: adding new photos, higher resolution

--- photo.php
+++ photo.php
@@ -1,17 +1,9 @@
 <?php
 $dir = "./comp_photos";
 
-//script de passage alÃ©atoire d'images.. yeah
-//$origin = substr($_SERVER['HTTP_REFERER'], 0,23);
-$origin = "g";
-$check = "http://www.arkaris.com/";
-
-
 header('Content-type: image/jpeg');
 header('Content-Disposition: attachment; filename="foto.jpg"'); 
 header('Cache-Control: no-store, no-cache, must-revalidate');
-
-
 
 $files = scandir($dir);
 $images = array();
@@ -26,16 +18,11 @@ foreach($files as $file)
 
 if (isset($_GET['n']) && $_GET['n'] <= count($images))
     $index = $_GET['n'];
+else if (function_exists('random_int'))
+    $index = random_int(1,count($images)) -1;
 else
     $index = mt_rand(1,count($images)) -1;
 
-
 $img = $images[$index];
 
-
-
-
-//print_r($images);
 readfile(sprintf("%s/%s",$dir,$img));
-?>
-
